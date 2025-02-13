6643b4cfc1a48526dbc03100dc166690e603b546
lucaspanzera <lucassouzapanzera@gmail.com>
2025-02-12 23:05:26 -0300

    Começando a Estudar Java

---
+++ src/ImprimindoVariaveis.java
@@ -0,0 +1,18 @@
+public class ImprimindoVariaveis {
+    public static void main(String[] args){
+        // Cometário de uma única linha
+
+        /*
+        Comentario mais de uma Linha
+         */
+
+        /***
+         * Comentario de Documentação
+         * @param args paramentro de entrada do metodo main
+         */
+        System.out.println("Dados impressos");
+        int idade = 10;
+        System.out.println(idade);
+        System.out.println("A idade é: " + idade);
+    }
+}
